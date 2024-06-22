4af621085a3f2f3a8eddfac323a28a7dd43fbfb5
zyf722 <1124982772@qq.com>
2024-06-22 14:45:28 +0800

    refactor: change args of create_api

--- clipper/api/__init__.py
+++ clipper/api/__init__.py
@@ -1,10 +1,16 @@
-from typing import Literal
+from typing import Dict, Literal
 
-from clipper.api.baidu import BaiduTranslationAPI, BaseTranslationAPI
+from clipper.api.baidu import BaiduTranslationAPI
+from clipper.api.base import BaseTranslationAPI
+from clipper.api.lingocloud import LingoCloudTranslationAPI
+
+__TRANS_API: Dict[str, BaseTranslationAPI] = {
+    "baidu": BaiduTranslationAPI,
+    "lingocloud": LingoCloudTranslationAPI,
+}
 
 
-def create_api(type: Literal["baidu"], appid: str, appkey: str) -> BaseTranslationAPI:
-    if type == "baidu":
-        return BaiduTranslationAPI(appid, appkey)
-    else:
+def create_api(type: Literal["baidu"], *args, **kwargs) -> BaseTranslationAPI:
+    if type not in __TRANS_API:
         raise ValueError(f"Unknown API type: {type}")
+    return __TRANS_API[type](*args, **kwargs)
